af5f77de2b57c5c22003f5e890ca9405e4d7839a
jimneylee <jimney@163.com>
2014-01-13 22:15:59 +0800

    finish login function

--- JLRubyChina/Models/OSCLoginModel.h
+++ JLRubyChina/Models/OSCLoginModel.h
@@ -8,10 +8,21 @@
 
 #import <Foundation/Foundation.h>
 #import "OSCAccountEntity.h"
+#import "OSCUserFullEntity.h"
+#import "OSCErrorEntity.h"
+#import "OSCNoticeEntity.h"
+
+typedef void (^ReturnBlock)(OSCUserFullEntity* userEntity, OSCErrorEntity* errorEntity);
 
 @interface OSCLoginModel : NSObject<NSXMLParserDelegate>
 
+@property (nonatomic, strong) OSCAccountEntity* accountEntity;
+@property (nonatomic, strong) OSCUserFullEntity* userEntity;
+@property (nonatomic, strong) OSCErrorEntity* errorEntity;
+@property (nonatomic, strong) OSCNoticeEntity* noticeEntiy;
+@property (nonatomic, strong) ReturnBlock returnBlock;
+
 - (void)loginWithUsername:(NSString*)username password:(NSString*)password
-                    block:(void(^)(OSCAccountEntity* user, NSError *error))block;
+                    block:(void(^)(OSCUserFullEntity* userEntity, OSCErrorEntity* errorEntity))block;
 
 @end
--- JLRubyChina/Models/OSCLoginModel.m
+++ JLRubyChina/Models/OSCLoginModel.m
@@ -13,6 +13,7 @@
 
 @interface OSCLoginModel()
 
+@property (nonatomic, copy) NSString* itemElementName;
 @property (nonatomic, copy) NSString* superElementName;
 @property (nonatomic, strong) NSMutableDictionary *currentDictionary;
 @property (nonatomic, strong) NSMutableString* tmpInnerElementText;//mutable must retain->www.stackoverflow.com/questions/3686341/nsmutablestring-appendstring-generates-a-sigabrt
@@ -24,9 +25,20 @@
 @implementation OSCLoginModel
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-- (void)loginWithUsername:(NSString*)username password:(NSString*)password
-                    block:(void(^)(OSCAccountEntity* user, NSError *error))block
+- (id)init
 {
+    self = [super init];
+    if (self) {
+        self.itemElementName = XML_USER;
+    }
+    return self;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)loginWithUsername:(NSString*)username password:(NSString*)password
+                    block:(void(^)(OSCUserFullEntity* userEntity, OSCErrorEntity* errorEntity))block
+{
+    self.returnBlock = block;
     NSString* path = [OSCAPIClient relativePathForSignIn];
     
     OSCAPIClient *httpClient = [[OSCAPIClient alloc] initWithBaseURL:[NSURL URLWithString:kAPIBaseURLString]];
@@ -35,6 +47,7 @@
     [params setObject:username forKey:@"username"];
     [params setObject:password forKey:@"pwd"];
     [params setObject:@"1" forKey:@"keep_login"];
+    self.accountEntity = [[OSCAccountEntity alloc] initWithUsername:username password:password];
     [httpClient postPath:path parameters:params
                                  success:^(AFHTTPRequestOperation *operation, id responseObject) {
                                      if ([responseObject isKindOfClass:[NSXMLParser class]]) {
@@ -45,13 +58,14 @@
                                      }
                                      else {
                                          if (block) {
-                                             NSError* error = [[NSError alloc] init];
-                                             block(nil, error);
+                                             OSCErrorEntity* errorEntity = [[OSCErrorEntity alloc] init];
+                                             block(nil, errorEntity);
                                          }
                                      }
                                  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                                      if (block) {
-                                         block(nil, error);
+                                         OSCErrorEntity* errorEntity = [[OSCErrorEntity alloc] init];
+                                         block(nil, errorEntity);
                                      }
                                  }];
         }
@@ -64,10 +78,18 @@
 //http://www.raywenderlich.com/zh-hans/36079/afnetworking%E9%80%9F%E6%88%90%E6%95%99%E7%A8%8B%EF%BC%881%EF%BC%89
 - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict
 {    
-    // TODO:news boday http://www.oschina.net/action/api/news_detail?id=47609
-    if ([elementName isEqualToString:XML_USER]) {
+    // set super element and create dic
+    if ([elementName isEqualToString:self.itemElementName]
+        || [elementName isEqualToString:XML_RESULT]
+        || [elementName isEqualToString:XML_NOTICE]) {
         self.currentDictionary = [NSMutableDictionary dictionary];
-        self.superElementName = XML_USER;
+        self.superElementName = elementName;
+    }
+    
+    // tmptext to store value in <e>value</e>
+    if ([self.superElementName isEqualToString:self.itemElementName]
+        || [self.superElementName isEqualToString:XML_RESULT]
+        || [self.superElementName isEqualToString:XML_NOTICE]) {
         self.tmpInnerElementText = [[NSMutableString alloc] init];;
     }
 }
@@ -82,24 +104,59 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
 {
-    if ([self.superElementName isEqualToString:self.detailItemElementName]) {
-        if (self.detailDictionary  && self.tmpInnerElementText) {
-            [self.detailDictionary setObject:self.tmpInnerElementText forKey:elementName];
+
+    // item user
+    if ([elementName isEqualToString:self.itemElementName]) {
+        // TODO: dirty code: set uid -> authorid, name -> author
+        if (self.currentDictionary[@"uid"]) {
+            [self.currentDictionary setObject:self.currentDictionary[@"uid"] forKey:@"authorid"];
+        }
+        if (self.currentDictionary[@"name"]) {
+            [self.currentDictionary setObject:self.currentDictionary[@"name"] forKey:@"author"];
+        }
+        self.userEntity = [OSCUserFullEntity entityWithDictionary:self.currentDictionary];
+        self.currentDictionary = nil;
+        self.superElementName = nil;
+    }
+    // get result error
+    else if ([elementName isEqualToString:XML_RESULT]) {
+        self.errorEntity = [OSCErrorEntity entityWithDictionary:self.currentDictionary];
+        self.currentDictionary = nil;
+        self.superElementName = nil;
+    }
+    // get notice
+    else if ([elementName isEqualToString:XML_NOTICE]) {
+        self.noticeEntiy = [OSCNoticeEntity entityWithDictionary:self.currentDictionary];
+        self.currentDictionary = nil;
+        self.superElementName = nil;
+    }
+    // set objects to item's dictionary
+    else if ([self.superElementName isEqualToString:self.itemElementName]
+        || [self.superElementName isEqualToString:XML_RESULT]
+        || [self.superElementName isEqualToString:XML_NOTICE]) {
+        if (self.currentDictionary && self.tmpInnerElementText) {
+            [self.currentDictionary setObject:self.tmpInnerElementText forKey:elementName];
         }
     }
+    
+    self.tmpInnerElementText = nil;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)parserDidEndDocument:(NSXMLParser *)parser
 {
-    self.topicDetailEntity = [OSCCommonDetailEntity entityWithDictionary:self.detailDictionary];
-    [super parserDidEndDocument:parser];
+    if (ERROR_CODE_SUCCESS == self.errorEntity.errorCode) {
+        [OSCAccountEntity storePassword:self.accountEntity.password
+                            forUsername:self.accountEntity.username];
+        if (self.returnBlock) {
+            self.returnBlock(self.userEntity, self.errorEntity);
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError
 {
-    [super parser:parser parseErrorOccurred:parseError];
 }
 
 @end
