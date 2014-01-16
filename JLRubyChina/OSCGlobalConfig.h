d3628263705ae19b3bb1d6c1788a1827268f77d8
jimneylee <jimney@163.com>
2014-01-16 21:11:02 +0800

    use NS_ENUM define enum type, more better

--- JLRubyChina/OSCGlobalConfig.h
+++ JLRubyChina/OSCGlobalConfig.h
@@ -11,29 +11,36 @@
 #import "OSCUserFullEntity.h"
 
 // 内容类型：资讯、博客、帖子、动弹（微博）类型
-typedef enum {
+typedef NS_ENUM(NSInteger, OSCContentType) {
     OSCContentType_LatestNews,//资讯  catalog:1
     OSCContentType_LatestBlog,//博客  catalog:none
     OSCContentType_RecommendBlog,//推荐阅读    catalog:none
     OSCContentType_Forum,//社区帖子    catalog:2
     OSCContentType_Tweet//社区帖子  catalog:3
-}OSCContentType;
+};
 
 // 社区活动类型
-typedef enum {
+typedef NS_ENUM(NSInteger, OSCForumTopicType) {
     OSCForumTopicType_QA,//问答
     OSCForumTopicType_Share,//分享
     OSCForumTopicType_Watering, //综合灌水
     OSCForumTopicType_Career,//职业
     OSCForumTopicType_Feedback//站务
-}OSCForumTopicType;
+};
 
 // 动弹类型
-typedef enum {
+typedef NS_ENUM(NSInteger, OSCTweetType) {
     OSCTweetType_Latest,//最新 uid=0
     OSCTweetType_Hot,//热门   uid=-1
     OSCTweetType_Mine,//我的  uid=
-}OSCTweetType;
+};
+
+typedef NS_ENUM(NSInteger, OSCCatalogType) {
+    OSCCatalogType_News=1,
+    OSCCatalogType_Forum=2,
+    OSCCatalogType_Tweet=3,
+    OSCCatalogType_Blog=-1
+};
 
 @interface OSCGlobalConfig : NSObject
 
@@ -44,12 +51,18 @@ typedef enum {
 // App Info
 + (NSString *)getIOSGuid;
 + (NSString *)getOSVersion;
++ (OSCCatalogType)catalogTypeForContentType:(OSCContentType)contentType;
 
 // Global UI
 + (MBProgressHUD*)HUDShowMessage:(NSString*)msg addedToView:(UIView*)view;
-+ (UIBarButtonItem*)createBarButtonItemWithTitle:(NSString*)buttonTitle Target:(id)target action:(SEL)action;
++ (UIBarButtonItem*)createBarButtonItemWithTitle:(NSString*)buttonTitle target:(id)target action:(SEL)action;
 + (UIBarButtonItem*)createMenuBarButtonItemWithTarget:(id)target action:(SEL)action;
 + (UIBarButtonItem*)createRefreshBarButtonItemWithTarget:(id)target action:(SEL)action;
 + (void)showLoginControllerFromNavigationController:(UINavigationController*)navigationController;
+
+// Emotion
++ (NSArray* )emotionsArray;
++ (NSString*)imageNameForEmotionCode:(NSString*)code;
++ (NSString*)imageNameForEmotionName:(NSString*)name;
 
 @end
